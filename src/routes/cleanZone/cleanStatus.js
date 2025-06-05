a04ec375c328201c40af0b153b0263408f6a99ef
juheeNoh123 <juhee10131013@gmail.com>
2025-06-05 23:30:09 +0900

    wait

--- src/routes/cleanZone/cleanStatus.js
+++ src/routes/cleanZone/cleanStatus.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const sendEmail = require('../../util/sendmail');
 const router = express.Router();
-router.get('/cleanzone/show/:groupId',async(req,res)=>{
-
+function getTodayDate() {
+    const today = new Date();
+    return today.toISOString().slice(0, 10);
+}
+cron.schedule('0 9 * * *', async () => {
+    const today = getTodayDate();
+    
 });
 module.exports = router;
