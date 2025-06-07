da01c072341619d2fbf849f57f4f12e3fd6ad7ab
Bucheo <atfqwe80@gmail.com>
2025-06-07 12:27:46 +0900

    ~

--- src/models/cleanboardModel.js
+++ src/models/cleanboardModel.js
@@ -1,16 +1,46 @@
 const db = require('../util/mysql')
 
+
 module.exports = class cleanboard {
-    //게시판 생성
-    static async save(imageName, cleanTime, content, member_id) {
-        return await db.execute(
-            'insert into cleanboard (imageName, cleanTime, content, member_id) values (?,?,?,?)',
-            [imageName, cleanTime, content, member_id]
+    //게시판 조회
+    static async findByMemberId(member_id){
+        const row =  await db.execute(
+            'select * from cleanBoard where member_id=?',
+            [member_id]
         );
+        return row[0];
     }
 
-    //게시판 조회
-    static async findByAllcleanboard(cleanBoard_id) {
+    static async save(cleanTime, content, member_id, group_id) {
+        return await db.execute(
+            'insert into cleanboard (cleanTime, content, member_id, usergroup_id) values (?,?,?,?)',
+            [cleanTime, content, member_id,group_id]
+        )
+
+    }
+
+    static async saveImage(cleanBoardId, image) {
+        return await db.execute(
+            'insert into cleanBoardImage (board_id, imageName) values (?,?)',
+            [cleanBoardId,image]
+        )
+
+    }
+
+    static async findImageByBoardId(cleanBoardId) {
+        const row= await db.execute(
+            'select * from cleanBoardImage where board_id=?',
+            [cleanBoardId]
+        );
+        return row[0];
+    }
+
+    static async findByMemberIdAndGroupId(member_id,group_id, start,end){
+        const row =  await db.execute(
+            'SELECT * FROM cleanBoard WHERE member_id = ? AND usergroup_id=? AND created_at BETWEEN ? AND ?',
+            [member_id,group_id,start,end]
+        );
+        return row[0];
 
     }
 }
